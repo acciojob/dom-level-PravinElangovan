8aa00487ec68c20b4a7ba36478e4ab4a5d65bc8e
PravinElangovan <115397005+PravinElangovan@users.noreply.github.com>
2023-04-14 13:03:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+var element = document.getElementById("level");
+
+// Initialize a counter variable to keep track of the DOM level
+var level = 0;
+
+// Use a loop to climb up the DOM tree and count the levels
+while (element) {
+  level++;
+  element = element.parentNode;
+}
+
+// Display the result using an alert box
+alert("The level of the element is: " + level);
